b8f06d886ca05e369154ef96683fe8099c295dc8
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-05 06:29:45 -0300

    Requisito 7 concluido

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+let contador = 0;
 function adicionarTarefa() {
+    
   const tarefa = document.getElementById('texto-tarefa').value;
   const lista = document.getElementById('lista-tarefas');
   const novaTarefa = lista.appendChild(document.createElement('li'));
   novaTarefa.innerText = tarefa;
+  novaTarefa.id = 'tarefa' + contador;
+  contador += 1;
   document.getElementById('texto-tarefa').value = "";
 }
+
+let lista = document.getElementById('lista-tarefas');
+lista.addEventListener('click', function(event) {
+    let evento = event.target.id;
+    let mudarBackground = document.getElementById(evento);
+    mudarBackground.style.backgroundColor = 'grey';
+})
