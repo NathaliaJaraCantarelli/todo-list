5b24289eeba9668d3b6d5827cb88524437a843fc
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-05 06:38:29 -0300

    Requisito 8 concluido

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ function adicionarTarefa() {
   const novaTarefa = lista.appendChild(document.createElement('li'));
   novaTarefa.innerText = tarefa;
   novaTarefa.id = 'tarefa' + contador;
+  novaTarefa.className = 'topicos-lista'
   contador += 1;
   document.getElementById('texto-tarefa').value = "";
 }
 
 let lista = document.getElementById('lista-tarefas');
 lista.addEventListener('click', function(event) {
+    let reset = document.querySelectorAll('.topicos-lista');
+    for(let index = 0; index < reset.length; index += 1) {
+        reset[index].style.backgroundColor = 'white';
+    }
     let evento = event.target.id;
     let mudarBackground = document.getElementById(evento);
     mudarBackground.style.backgroundColor = 'grey';
